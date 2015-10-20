730d719ec7b6ff140e0eb3413056ec38c4de31c2
Adrian Li <li.adrianmc@gmail.com>
2015-10-20 17:40:09 +0800

    tooltips

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
- $('.view-sources').click(function(){
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
+
+  $('.disabled').click(function(e){
+    e.preventDefault();
+  });
+
+  $('.view-sources').click(function(){
     if ($(".sources").hasClass('hidden')) {
       $(".sources").removeClass('hidden');
     } else {

67793f85f1ccff82eb58664c9497168e7b4f79a4
HL <nunavutaccountant2@gmail.com>
2016-03-10 00:37:18 +0800

    Fixed old react router ref

--- app.js
+++ app.js
@@ -551,8 +551,6 @@ var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
 
-var _reactRouter = require('react-router');
-
 var _Problem = require('./Problem');
 
 var _Problem2 = _interopRequireDefault(_Problem);
